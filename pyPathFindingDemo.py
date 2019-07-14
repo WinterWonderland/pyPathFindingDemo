b5eb5086593dc93fb579194fe5e93f7812cd3f50
Klaus Winter <software@winterwonderlan.de>
2019-07-14 22:31:25 +0200

    first implementation of search algorithm with visualization

--- pyPathFindingDemo.py
+++ pyPathFindingDemo.py
@@ -1,7 +1,8 @@
-from game import Board
+from game import Board, SearchOptimization
 
-board = Board(width = 2048, 
-              height = 1024, 
-              percentage_of_wall_fields = 0.2, 
-              fps=30)
+board = Board(width=2048, 
+              height=1024, 
+              percentage_of_wall_fields=0.2, 
+              fps=30,
+              optimization=SearchOptimization.greedy)
 board.run()
